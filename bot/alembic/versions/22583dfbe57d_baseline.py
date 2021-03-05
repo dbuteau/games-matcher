c9b2d1c88dd855eb02efb444eaf8c1581319c48a
Buteau Daniel <daniel.buteau@gmail.com>
2021-03-05 13:21:40 +0100

    fix problem with upgrade bdd

--- bot/alembic/versions/22583dfbe57d_baseline.py
+++ bot/alembic/versions/22583dfbe57d_baseline.py
@@ -6,6 +6,7 @@ Create Date: 2021-03-02 18:27:48.695163
 
 """
 from alembic import op
+from sqlalchemy.engine.reflection import Inspector
 import sqlalchemy as sa
 
 
@@ -15,41 +16,51 @@ down_revision = None
 branch_labels = None
 depends_on = None
 
+conn = op.get_bind()
+inspector = Inspector.from_engine(conn)
+tables = inspector.get_table_names()
 
 def upgrade():
-    op.create_table(
-      'Games',
-      sa.Column('game_id',    sa.Integer, primary_key=True, unique=True, autoincrement=True),
-      sa.Column('name',       sa.String(100), unique=True, nullable=False),
-      sa.Column('igdb_id',    sa.Integer, unique=True, nullable=True),
-      sa.Column('steam_id',   sa.Integer, unique=True, nullable=True),
-      sa.Column('discord_id', sa.Integer, unique=True, nullable=True),
-      sa.Column('multiplayer',sa.Boolean, nullable=True)
-    )
+    if 'Games' not in tables:
+      op.create_table(
+        'Games',
+        sa.Column('game_id',    sa.Integer, primary_key=True, unique=True, autoincrement=True),
+        sa.Column('name',       sa.String(100), unique=True, nullable=False),
+        sa.Column('igdb_id',    sa.Integer, unique=True, nullable=True),
+        sa.Column('steam_id',   sa.Integer, unique=True, nullable=True),
+        sa.Column('discord_id', sa.Integer, unique=True, nullable=True),
+        sa.Column('multiplayer',sa.Boolean, nullable=True)
+      )
 
-    op.create_table(
-      'Users',
-      sa.Column('user_id',          sa.Integer, primary_key=True),
-      sa.Column('disallow_globally',sa.Boolean, default=False),
-      sa.Column('disallow_users',   sa.UnicodeText, default=None)
-    )
+    if 'Users' not in tables:
+      op.create_table(
+        'Users',
+        sa.Column('user_id',          sa.Integer, primary_key=True),
+        sa.Column('disallow_globally',sa.Boolean, default=False),
+        sa.Column('disallow_users',   sa.UnicodeText, default=None)
+      )
 
-    op.create_table(
-      'Servers',
-      sa.Column('server_id', sa.Integer, primary_key=True),
-      sa.Column('prefix',    sa.String(1), default="$")
-    )
-    op.create_table(
-      'UserGames',
-      sa.Column('user_id',        sa.Integer, sa.ForeignKey('Users.user_id'), autoincrement=False, index=True, primary_key=True),
-      sa.Column('game_id',        sa.Integer, sa.ForeignKey('Games.game_id'), autoincrement=False, index=True, primary_key=True),
-      sa.Column('last_played_at', sa.DateTime, nullable=True)
-    )
-    op.create_table(
-      'WhosUp',
-      sa.Column('user_id', sa.Integer, sa.ForeignKey('Users.user_id'), primary_key=True),
-      sa.Column('expire_at', sa.DateTime)
-    )
+    if 'Servers' not in tables:
+      op.create_table(
+        'Servers',
+        sa.Column('server_id', sa.Integer, primary_key=True),
+        sa.Column('prefix',    sa.String(1), default="$")
+      )
+
+    if 'UserGames' not in tables:
+      op.create_table(
+        'UserGames',
+        sa.Column('user_id',        sa.Integer, sa.ForeignKey('Users.user_id'), autoincrement=False, index=True, primary_key=True),
+        sa.Column('game_id',        sa.Integer, sa.ForeignKey('Games.game_id'), autoincrement=False, index=True, primary_key=True),
+        sa.Column('last_played_at', sa.DateTime, nullable=True)
+      )
+
+    if 'WhosUp' not in tables:
+      op.create_table(
+        'WhosUp',
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('Users.user_id'), primary_key=True),
+        sa.Column('expire_at', sa.DateTime)
+      )
 
 def downgrade():
     op.drop_table('Games')
